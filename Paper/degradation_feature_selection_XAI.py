9613c776be69bb3154cf476d5f2ad9e23590e166
JollonLD <jonas.durao@unifesp.br>
2025-10-15 13:43:53 -0300

    update

--- Paper/degradation_feature_selection_XAI.py
+++ Paper/degradation_feature_selection_XAI.py
@@ -444,7 +444,7 @@ def run_train_degradation():
                 'features_selected': df_desc.columns[selected_original_indices].tolist(),
                 'original_indices': selected_original_indices.tolist(),
                 'test_loss': test_loss,
-                'shap_explanation': shap_explanation.tolist() if shap_explanation is not None else None,
+                'shap_explanation': shap_explanation if shap_explanation is not None else None,
                 'positive_fidelity': float(pos_fidel) if pos_fidel is not None else None,
                 'negative_fidelity': float(neg_fidel) if neg_fidel is not None else None
             })
